acb6ef3e1e112c89f475959e50b3cba6c6102e45
nora-sch <nora.schumann.development@gmail.com>
2020-12-03 08:54:47 +0100

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="mb-4">Un petit quiz</h1>
+    <b-alert v-if="fin" show="">Votre score est : {{ score }} / {{ questions.length }}</b-alert>
+    <b-card :header="questions[index].question"
+            header-tag="header">
+      <b-list-group>
+        <b-list-group-item
+            button
+            v-for="(item, index) in questions[index].answers"
+            :key="item.id"
+            @click="action(index)"
+            :variant="variants[index]">
+          {{ item }}
+        </b-list-group-item>
+      </b-list-group>
+      <b-button v-if="fin" @click="recommencer" class="mt-4">Recommencer !</b-button>
+      <b-button v-if="voirReponse && !fin" @click="continuer" class="mt-4">Continuer !</b-button>
+    </b-card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data: function () {
+    return {
+      fin: false,
+      index: 0,
+      score: 0,
+      variants: [...Array(4)],
+      voirReponse: false,
+    }
+  },
+  methods: {
+    action: function (index) {
+      // Test bonne réponse
+      if (index === this.questions[this.index].ok) {
+        this.score++;
+      } else {
+        this.variants[index] = 'danger';
+      }
+      this.voirReponse = true;
+      this.variants[this.questions[this.index].ok] = 'success';
+      if (this.index === this.questions.length - 1) {
+        this.fin = true;
+      }
+    },
+    recommencer: function () {
+      this.voirReponse = this.fin = this.index = this.score = 0;
+      this.variants = [...Array(4)];
+    },
+    continuer: function () {
+      this.voirReponse = false;
+      this.variants = [...Array(4)];
+      this.index++;
+    }
+  },
+  computed: {
+    questions() {
+      return this.$store.state.questions;
+    }
+  },
+  created() {
+    this.$store.dispatch('getData');
   }
 }
 </script>
@@ -26,3 +77,7 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+
+
